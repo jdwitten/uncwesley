a712821b1ce393907d4aff411e3d1883f4d01a7f
jdwitten <jdwitten11@gmail.com>
2016-08-03 20:16:44 -0400

    Rename test.php to add_test.php

---
+++ add_test.php
@@ -0,0 +1,21 @@
+<?php
+
+ $charset = 'utf8';
+      $host = "us-cdbr-azure-east-c.cloudapp.net";
+      $user = "bddfe4567fcee0";
+      $pass = "aaa47743";
+      $db = "uncwesley";
+      $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+      $opt = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+      ];
+      $pdo = new PDO($dsn, $user, $pass, $opt);
+      $query =   $pdo->prepare("SELECT COUNT(*) FROM uncwesley.users");
+      $query->execute();
+      $arr = $query->fetch(PDO::FETCH_ASSOC);
+      
+      var_dump($arr);
+
+?>
