15c9d86b0b8d211464726dbcf7bf15f807591ae6
jdwitten <jdwitten11@gmail.com>
2016-09-14 09:17:22 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,4 @@
 
-var MyComponentClass = React.createClass({
-  render: function () {
-    return (<h1>Hello world</h1>);
-  }
-});
-
 ReactDOM.render(
   <h1>Hello, World!</h1>,
   document.getElementById('app')
