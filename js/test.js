fb17c403af9fa12176621b2f1776d6c97c1003ac
jdwitten <jdwitten11@gmail.com>
2016-09-15 16:03:35 -0400

    test

--- js/test.js
+++ js/test.js
@@ -1,33 +1,37 @@
 var Example = React.createClass({
+    componentDidMount(){
+        this.search("http://uncwesley.azurewebsites.net/Get_Prayers.php")
+    },
+    
     getInitialState: function(){
       return{
-          prayerData: ["Waiting for AJAX"]
+          prayerData: [{content:"Waiting for AJAX"}]
       }  
     },
-  componentDidMount: function(){
-      var xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-            var prayers = this.responseText
-            var prayerContent = []
-            for(var i=0; i<prayers.length;i++){
-                console.log(prayers[i]["content"])
-              prayerContent.push(prayers[i]["content"])
-            }
-            console.log(prayerContent)
-            setState({
-                prayerData: prayerContent 
-            })
-      }
-  };
-  xhttp.open("GET", "Get_Prayers.php", true);
-  xhttp.send();
-  return <h1>Waiting for AJAX</h1>
+    showResults: function(response){
+        this.setState({
+            prayerData: response.results
+        })
+    }
   },
+    search: function(URL){
+        $.ajax({
+            type:"GET",
+            datatype:'jsonp',
+            url:URL,
+            success: function(response){
+                this.showResults(response);
+            }.bind(this)
+        })
+    }
     
     render: function(){
+        var content = []
+        for(var i=0; i<this.state.prayerData.length; i++){
+           content.push(this.state.prayerData.length); 
+        }
         return(
-            <ul>{this.state.prayerData}</ul>
+            <ul>{content}</ul>
         )
     }
     
